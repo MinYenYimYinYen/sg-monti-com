93c447ea67895ebd4854f90818dbd11d8b609af7
LukeHolker <lukeholker@gmlawncare.onmicrosoft.com>
2026-01-15 10:02:46 -0600

    Configure Prettier, enhance TypeScript settings, update ESLint.
    Added `.idea/prettier.xml` for Prettier configuration. Enhanced `tsconfig.json` with stricter TypeScript checks and additional options such as `sourceMap` and `baseUrl`. Updated ESLint configuration to support `eslint-plugin-react-compiler` and adjusted rules for better TypeScript and React handling.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactCompiler from "eslint-plugin-react-compiler";
+
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    {
+        plugins: {
+            "react-compiler": reactCompiler,
+        },
+        rules: {
+            "react-compiler/react-compiler": "error",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "warn", // or "error"
+                {
+                    "argsIgnorePattern": "^_",
+                    "varsIgnorePattern": "^_",
+                    "caughtErrorsIgnorePattern": "^_",
+                    "ignoreRestSiblings": true
+                }
+            ],
+        },
+    },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
 ]);
 
 export default eslintConfig;
